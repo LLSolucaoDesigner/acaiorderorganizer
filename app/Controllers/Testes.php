730e8a8a27ebb4f6c93d61b5c8833a22095ea764
LLSolucaoDesigner <llsolucaodesigner@gmail.com>
2022-09-23 22:37:11 -0300

    first commit

---
+++ app/Controllers/Testes.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class Testes extends BaseController
+{
+	public function index()
+	{
+
+		$data = [
+			'titulo' => 'Curso de como fazer um sistema de entrega de comida com codeigniter 4',
+			'subtitulo' => 'Muito massa conhecer a nova versão do Codeigniter 4',
+		];
+
+		return view('Testes/index', $data);
+	}
+
+	public function novo() 
+	{
+		echo 'Esse é mais um método do controller Testes';
+	}
+}
